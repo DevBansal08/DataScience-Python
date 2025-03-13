158d0116ee4c42e4ac3c31c698d8699920ed9b01
DevBansal08 <devb6646@gmail.com>
2025-03-13 23:01:28 +0530

    basics of numpy

--- Numpy/basic_array.py
+++ Numpy/basic_array.py
@@ -1,8 +1,12 @@
 import numpy as np
-a1 = np.array([3,2,5,6])
+# a1 = np.array([3,2,5,6])
 
-print(a1 , a1.ndim)
-a = np.ones ((3,4))
-print (a)
+# print(a1 , a1.ndim)
+# a = np.ones ((3,4))
+# print (a)
 
-a3 = np.arange(1,6)
+# a3 = np.arange(1,6)
+ 
+aA= np.array([2,3,4])
+aB= np.array([4,5,6])
+print(aA+aB)
---
+++ Numpy/basic_array2.py
@@ -0,0 +1,7 @@
+import numpy as np
+
+a = np.array([1,2,3])
+print(a.ndim) 
+
+b = np.array([[1,3,5], [23,3,4], [3,4,5]])
+print(b.ndim)
